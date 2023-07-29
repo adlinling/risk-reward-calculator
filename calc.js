42f84a2084faa09fc8a269117833d3052d634eb2
Adlin Ling <3029324+adlinling@users.noreply.github.com>
2023-07-29 02:13:07 -0500

    Moved JS code to separate file

---
+++ calc.js
@@ -0,0 +1,20 @@
+
+function calculate(){
+
+
+	var accountsize = document.getElementById("accountsize").value;
+	var percent = document.getElementById("riskpercent").value;
+	var riskpercent = percent/100;
+
+	var entryprice = document.getElementById("entryprice").value;
+	var stopprice = document.getElementById("stopprice").value;
+	var targetprice = document.getElementById("targetprice").value;
+
+	var riskrange= entryprice - stopprice;
+	var rewardrange= targetprice - entryprice;
+
+
+	document.getElementById("shares").innerHTML = "display number of shares";
+	document.getElementById("ratio").innerHTML = "display ratio";
+
+}
